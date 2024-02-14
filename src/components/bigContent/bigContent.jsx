fc4a398fa85e89cf10187a4d94815e74d456b310
Thomas Musial <th896493@dal.ca>
2024-02-14 13:20:26 -0400

    added components

---
+++ src/components/bigContent/bigContent.jsx
@@ -0,0 +1,101 @@
+import React,{useState,useEffect} from "react";
+
+// import TextFormat from "../textFormat";
+import './bigContent.css'
+import { motion } from "framer-motion";
+import { useRef } from "react";
+import reactLogo from '../../media/react-logo.webp'
+
+
+const BigContent = ({reverse,id,image,
+customText}) => {
+
+    const [isAnimated, setIsAnimated] = useState(false)
+
+    const ref = useRef()
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const contentElement = ref.current;
+            const elementTop = contentElement.getBoundingClientRect().top;
+         
+            const windowHeight = window.innerHeight;
+    
+            const offset = 350;
+    
+            if (elementTop < windowHeight - offset) {
+                setIsAnimated(true);
+             
+            }
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+    
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [id]);
+
+
+    const animations = {
+        initial:{
+            x: reverse ? -50 : 50,
+            opacity:0
+        },
+        animate:{
+            x:0,
+            opacity:1,
+            transition:{
+                opacity: '0.3s ease-in'
+            }
+        }
+
+    }
+
+
+    return (
+        <div className="big-content-container"
+        ref={ref}
+        id={id}>
+                <div className={`content-text-container ${reverse ? 'reverse-container' : ''}`}>
+                    <div 
+                    className={!reverse ? 'text-container' : 'reverse-text'}>
+                        {customText ? (
+                            <>
+                            {customText}
+                            </>
+                        ):(
+                            <>
+                        
+                            <h2 className="title-text">
+                            2021 Campaign Worker
+                         </h2>
+                         <p className="description-text">
+                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita quas minima sapiente unde ipsam inventore enim ducimus.
+                         </p>
+                         <ul>
+                             <li>4 courses in Java</li>
+                             <li>Studies</li>
+                             <li>Info</li>
+                             <li>Data</li>
+                             <li>Infographics</li>
+                         </ul>
+                         </>
+                        )}
+                      
+ 
+
+                    </div>
+                    <div className={!reverse ? 'image-container' : 'image-container reverse-image' }>
+                            <motion.img src={image}
+                            initial={animations.initial}
+                            animate={isAnimated ? animations.animate : animations.initial}
+                            />
+                    </div>
+
+                </div>
+        </div>
+    )
+}
+
+export default BigContent
---
+++ src/components/herobanner2/herobanner2.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import fullBody from '../../media/smiley-tom-half.png'
+import './herobanner2.css'
+const Herobanner2 = () => {
+
+    
+
+    return (
+        <div className="herobanner2-container">
+
+            <div className="hero2-content-box">
+                <div className="hero2-text-box">
+                 <h2>Hello, I'm Thomas</h2>
+                 <h1 >A React JS Developer</h1>
+                 <p>
+                 I take a disciplined and focused approach to creating unique,
+                 interactive and functional applications.
+                 </p>
+                 <button>
+                Learn More
+            </button>
+                </div>
+                <img className="hero2-image"
+                src={fullBody}/>
+            </div>
+   
+            
+        </div>
+    )
+}
+
+export default Herobanner2
