635ed076ab60faac1badc4f2ed89725819c1a9df
Thomas Musial <th896493@dal.ca>
2024-02-16 09:47:56 -0400

    website looking somewhat presentable

---
+++ .history/src/data/data_20240216085234.js
@@ -0,0 +1,126 @@
+
+
+
+export const designAdvantages = [
+      {
+   
+        name: 'Designing Reuseable Components',
+        description: `I have created about reuseable components
+        during my time in react that are ready to be used at anytime.
+        In fact, everything on this website was made during other projects
+        and simply imported to make this portfolio.`,
+      },
+      {
+
+        name: 'Visually Appealing Interfaces',
+        description: `I have scoured the internet to find some of the nicest
+        websites and emulate them with my own style and combing other
+        components from multiple websites with each other
+       along with years of drawing experience to make websites that stand out among the rest.`,
+      },
+      {
+  
+        name: 'Hooks',
+        description: `Through out my projects, especially my card game, I had to learn how to
+        use useState, useEffect and useContext thoroughly to ensure my applications worked properly.
+        This included keeping track of the score and time in my card game, knowing where a specific element
+        is relative to the viewport to enable an animation and 
+        knowing where the text was relative to the video
+        to enable and disable overflow to hidden on the component
+        that has the video showcasing my coding projects.`,
+      },
+      {
+
+        name: 'Customizable Components',
+        description: `Within the components I've made, some of them have alternate attributes and qualities so they can look different while still being from the same component, increasing efficiency and diversity of the page.\n
+        An example of this is this component you're viewing right now. Both the tilting feature and animation can be toggled on and off by simply putting two props to false, and this list that you are viewing right now can be any form a text you want, you can put it into the customText prop to have customizable text or a regular p tag.`,
+      },
+      {
+
+        name: 'Conditional Rendering',
+        description: `My knowledge in conditional rendering is one of the biggest
+        factors in creating these dynamic interfaces, the text you are reading right now
+        has been dynamically rendered itself. I have a great grasp
+        of how to use conditionals in return statements and on style consts
+        to rendering certain pieces based if another condition is true, like the 
+        custom text, make a container of height 0 when it is not clicked on
+        and height of 200 when clicked and much more.`,
+      },
+      {
+        name: 'CSS',
+        description: `Through out my javascript development my css has improved a lot as well,
+        throughout my projects I learned how to use grid and flex layouts to display the cards properly
+        in my card game, do responsive web, especially going from flex direction column for mobile
+        and then row for desktop, and adding hover effects.\n\n This also includes advanced selectors like .class1 .class2 for
+        precise element targeting and using the "id" tag
+        to overwrite general css when a specific element needs specific editing`,
+      },
+    ]
+
+
+    export const designAdvantages2 = [
+        {
+     
+          name: 'Java',
+          description: `I have taken 4 courses in Java over 2
+          years at Dalhousie where I learned object orientated programming,
+          data structures and algorithms. `,
+        },
+        {
+  
+          name: 'C',
+          description: `I have made arraylists, linked lists, stacks, queues
+          binary search trees and heaps all in C. This gave me a thorough understanding of
+          low level programming and data structures`,
+        },
+   
+        {
+  
+          name: 'General design skills',
+          description: `I have been into design since I was
+          young and before coding I have years of experience drawing
+          things and creating content through drawing, making videos and
+          animations. I have keen visual intelligence so I understand
+          how to make appealing and unique user interfaces.`,
+        },
+        {
+          name:"A lot of discipline"
+        }
+       
+      ]
+
+
+      export const project1 =
+      {
+        subtitle:'My first react js project',
+        title:'The Quantum Card Game',
+        description:`My first ever project in react, a card game with art that I made where you can test your memory and reaction time.
+        This project taught me a lot of how to use hooks like useState, useEffect and context
+        to ensure the game functioned properly`
+      }
+
+      export const project2 = {
+        subtitle:'My freelancing website',
+        title:'Q3 Designs',
+        description:"This is where I offer freelancing for unique and visually appealing websites. "+
+        'In this project I learned a lot about how to make unique and visually appealing interfaces '+
+        'and make reuseable components'
+      }
+
+     export const hydrolistIntro1 = {
+        title:'My Primary Skill',
+        description:`Currently my I am most competent in react JS
+        with over 550 hours since July 2023 (Approximately 4 hours of coding a day everyday). Here are some of my skills within react,
+        which include html and css knowledge as well`
+      }
+
+      export const hydrolistIntro2 = {
+        title:'My secondary skills',
+        description:'Besides react js I have two years of computer science at Dalhousie University, here'+
+        ' are some other skills I have'
+      }
+  
+    
+  
+
+
